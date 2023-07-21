11cb6469f3df4b3d36e4cf415a54b5fdd38d30c6
Sean Meng <sean.meng.fv@renesas.com>
2023-07-21 18:46:40 +0000

    Update repo for llhttp fix

--- FreeRTOS-Plus/Source/Application-Protocols/network_transport/transport_mbedtls.h
+++ FreeRTOS-Plus/Source/Application-Protocols/network_transport/transport_mbedtls.h
@@ -60,6 +60,12 @@
 extern void vLoggingPrintf( const char * pcFormatString,
                             ... );
 
+/* Map the SdkLog macro to the logging function to enable logging
+ * on Windows simulator. */
+#ifndef SdkLog
+    #define SdkLog( message )    vLoggingPrintf message
+#endif
+
 #include "logging_stack.h"
 
 /************ End of logging configuration ****************/
