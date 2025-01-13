ac32a789955f762bed2fe95ddd85d3b17865daf8
Sean Meng <sean.meng.fv@renesas.com>
2025-01-13 12:22:12 +0000

    revert to default config

--- FreeRTOS-Plus/Source/Application-Protocols/network_transport/mbedtls_bio_tcp_sockets_wrapper.c
+++ FreeRTOS-Plus/Source/Application-Protocols/network_transport/mbedtls_bio_tcp_sockets_wrapper.c
@@ -31,7 +31,7 @@
 
 /* MbedTLS includes. */
 #if !defined( MBEDTLS_CONFIG_FILE )
-    #include "mbedtls/config.h"
+    #include "mbedtls/mbedtls_config.h"
 #else
     #include MBEDTLS_CONFIG_FILE
 #endif
